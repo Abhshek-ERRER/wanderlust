01bb40d8412c3907c6d594e7e08450d42d478f0f
Abhshek-ERRER <abhishekrajbhar2022@gmail.com>
2026-09-12 14:01:12 +0530

    change o with  a

--- cloudConfig.js
+++ cloudConfig.js
@@ -11,7 +11,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'wonderlust_DEV',
+    folder: 'wanderlust_DEV',
     allowFormat: ["png","jpg","jpeg"],
   },
 });
